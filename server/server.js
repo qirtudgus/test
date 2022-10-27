3fd61fa69f54ea6f249b3d3f54546470718a67c0
qirtudgus <qirtudgus@naver.com>
2022-10-28 01:53:40 +0900

    mysql db 테스트

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import cors from "cors";
+import { db } from "./db.js";
 const SERVER_PORT = 3002;
+
+db.connect((err) => {
+  if (err) console.log("MySQL 연결 실패 : ", err);
+  console.log("MySQL Connected!!!");
+});
 
 const app = express();
 app.use(cors());
@@ -8,9 +14,15 @@ app.use(express.json());
 
 app.post("/api/test", (req, res) => {
   const a = req.body;
+  let dbTest;
   console.log(a);
 
-  res.status(200).json({ test: "테스트" });
+  db.query("SELECT * FROM testTable", [], (err, rows) => {
+    console.log(rows);
+    dbTest = rows;
+  });
+
+  res.status(200).json({ test: "테스트", db: dbTest });
 });
 
 app.listen(SERVER_PORT, () => {
