e763ff2143c30d41ecfee982854cb0a864470718
qirtudgus <qirtudgus@naver.com>
2022-10-28 01:11:38 +0900

    api 테스트

--- src/App.js
+++ src/App.js
@@ -2,23 +2,57 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 function App() {
+  const 로컬호출 = () => {
+    axios({
+      url: "http://localhost:3002/api/test", // 통신할 웹문서
+      method: "post", // 통신할 방식
+      data: {
+        // 인자로 보낼 데이터
+        foo: "diary",
+      },
+    }).then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  const 아이피호출 = () => {
+    axios({
+      url: "http://54.180.46.178:3002/api/test", // 통신할 웹문서
+      method: "post", // 통신할 방식
+      data: {
+        // 인자로 보낼 데이터
+        foo: "diary",
+      },
+    }).then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  const 도메인호출 = () => {
+    axios({
+      url: "https://sungtt.com/api/test", // 통신할 웹문서
+      method: "post", // 통신할 방식
+      data: {
+        // 인자로 보낼 데이터
+        foo: "diary",
+      },
+    }).then((res) => {
+      console.log(res.data);
+    });
+  };
+
   return (
-    <button
-      onClick={() => {
-        axios({
-          url: "http://localhost:3002/api/test", // 통신할 웹문서
-          method: "post", // 통신할 방식
-          data: {
-            // 인자로 보낼 데이터
-            foo: "diary",
-          },
-        }).then((res) => {
-          console.log(res.data);
-        });
-      }}
-    >
-      API테스트
-    </button>
+    <>
+      <button onClick={로컬호출}>
+        http://localhost:3002/api/test API테스트
+      </button>
+      <button onClick={아이피호출}>
+        http://54.180.46.178:3002/api/test API테스트
+      </button>
+      <button onClick={도메인호출}>
+        https://sungtt.com/api/test API테스트
+      </button>
+    </>
   );
 }
 
